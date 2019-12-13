5078414e705a24a579d458b12973be2d5689f2ff
danil <reloaderlev@gmail.com>
2019-12-13 13:35:25 +0200

    change gitignore

---
+++ build/precache-manifest.1230dc3e812039eea95e77067b69f852.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "051796a8fdbaf03eb14b7eb5df0bc68c",
+    "url": "/react-hw4-1/index.html"
+  },
+  {
+    "revision": "a2daaf6d75b9c05b41f8",
+    "url": "/react-hw4-1/static/css/moviesDetailsPage.9d31953c.chunk.css"
+  },
+  {
+    "revision": "9c20047592e31c2d562b",
+    "url": "/react-hw4-1/static/css/reviewsPage.1a721ce7.chunk.css"
+  },
+  {
+    "revision": "5544eddadbd7931766ac",
+    "url": "/react-hw4-1/static/js/0.cf6bbc25.chunk.js"
+  },
+  {
+    "revision": "3df54bba2137ec524f3fb39f2c61461a",
+    "url": "/react-hw4-1/static/js/0.cf6bbc25.chunk.js.LICENSE"
+  },
+  {
+    "revision": "fd6867fff8d1582284a1",
+    "url": "/react-hw4-1/static/js/8.46212432.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/react-hw4-1/static/js/8.46212432.chunk.js.LICENSE"
+  },
+  {
+    "revision": "850175e200b6da2bc971",
+    "url": "/react-hw4-1/static/js/castPage.8e1505bc.chunk.js"
+  },
+  {
+    "revision": "a763147c0aa5aa44cc9b",
+    "url": "/react-hw4-1/static/js/homePage.3ca14060.chunk.js"
+  },
+  {
+    "revision": "61d4440c995275178be3",
+    "url": "/react-hw4-1/static/js/main.5ea042a5.chunk.js"
+  },
+  {
+    "revision": "a2daaf6d75b9c05b41f8",
+    "url": "/react-hw4-1/static/js/moviesDetailsPage.06245945.chunk.js"
+  },
+  {
+    "revision": "674458e23e06a631ab6c",
+    "url": "/react-hw4-1/static/js/moviesPage.151a8b61.chunk.js"
+  },
+  {
+    "revision": "9c20047592e31c2d562b",
+    "url": "/react-hw4-1/static/js/reviewsPage.406bef0e.chunk.js"
+  },
+  {
+    "revision": "41a02a08f9d4d4b2bf44",
+    "url": "/react-hw4-1/static/js/runtime-main.18181dd8.js"
+  }
+]);
